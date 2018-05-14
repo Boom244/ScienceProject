1d16faf6848fa1a90c68dd504a5cc67f7533a297
Boom244 <bolumsba@gmail.com>
2018-05-13 17:08:20 -0700

    Added Functionality on CreatorTool end
    I added functionality on CreatorTool's end, so that's me getting back into scripting.

--- PhysicsTest/Assets/CreateObjects.cs
+++ PhysicsTest/Assets/CreateObjects.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
+﻿	using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CreateObjects : MonoBehaviour {
-
+	public Camera createCam; 
+	public float RangeInt = 20f;
+	public GameObject spawnui;
+	public PrimitiveType PrimToSpawn;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			SpawnObj();
+		}
+
+		if (Input.GetButtonDown("Fire2"))
+		{
+			spawnui.gameObject.SetActive(true);
+		}
+	}
+	public void SpawnObj() 
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (createCam.transform.position, createCam.transform.forward, out hit, RangeInt) && PrimToSpawn != null) 
+		{
+			GameObject.CreatePrimitive (PrimToSpawn);
+		}
+
+
 	}
 }
